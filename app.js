76e6c4a0b0f2a15ee16a0f667662732c34ad988e
Olena <hbrysh95@gmail.com>
2021-08-02 21:11:07 +0200

    added logic that implements saving tabs value into the localStorage and rendering it on the screen

--- app.js
+++ app.js
@@ -4,18 +4,13 @@ const saveInputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
 const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const saveTabBtn = document.getElementById("tab-btn")
 
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage
   render(myLeads)
 }
-
-saveISnputBtn.addEventListener("click", function() {
-  myLeads.push(inputEl.value)
-  inputEl.value = ""
-  render(myLeads)
-})
-
+// the piece of the logic which allows to render input's/ tab's values on the screen
 function render(leads) {
   let listItems = ""
   for (let i = 0; i < leads.length; i++) {
@@ -29,16 +24,25 @@ function render(leads) {
   }
     ulEl.innerHTML = listItems  
 }
-
+// save input value into the localStorage and render it on the screen
+saveInputBtn.addEventListener("click", function() {
+  myLeads.push(inputEl.value)
+  inputEl.value = ""
+  localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+  render(myLeads)
+})
+// save the current tab into the localStorage and render its value on the screen
+saveTabBtn.addEventListener("click", function(){    
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+      myLeads.push(tabs[0].url)
+      localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+      render(myLeads)
+  })
+})
+// clear the localStorage and delete items that were rendered on the screen before
 deleteBtn.addEventListener("click", function() {
   localStorage.clear()
   myLeads = []
   render(myLeads)
 })
 
-inputBtn.addEventListener("click", function() {
-  myLeads.push(inputEl.value)
-  inputEl.value = ""
-  localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-  render(myLeads)
-})
